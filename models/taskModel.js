148a53e683b97b63a672073e14b8c6b08cad4ad9
lequangdung <ledung2841998@gmail.com>
2022-11-18 12:19:53 +0700

    change db

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql2');
 var con = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "280498",
-    database: "courselistdata"
+    host: "sinno.soict.ai",
+    user: "subjects_bdc",
+    password: "lequangdung",
+    database: "subjects_bdc"
 });
 con.connect(function(err) {
     if (err) throw err;
