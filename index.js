a223c46d762681667e91203450312f6fd2bd0ef8
lequangdung <ledung2841998@gmail.com>
2022-11-18 09:57:46 +0700

    change port

--- index.js
+++ index.js
@@ -184,6 +184,6 @@ app.post('/test', (req, res) => {
 });
 
 // port where app is served
-app.listen(3000, () => {
-    console.log('The web server has started on port 3000');
+app.listen(70, () => {
+    console.log('The web server has started on port 70');
 });
